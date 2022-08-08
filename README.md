de6909dbd2aa235cacf1c63ceeb26f0d32c60b58
haaminyangibo1 <87262980+haaminyangibo1@users.noreply.github.com>
2022-08-08 17:32:36 +0100

    Ansers

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# fetch-bootcamp-w6-d1
+Lets create a web page for jokes
+
+npm init
+npm install
+
+Using the Joke API, fetch the data and display a different joke with each click of the button.
+
+I've got the URL for jokes, in index.js so use that to get the data!
+
+
+Once you've got the data, using the methods we have used before, input the data onto the html site so it shows a new joke with every click of the button.
+
+There is already an HTMl file available 
+
+
+
+EXTENSION.
+
+Play around with the Joke API, display different jokes, categories and amounts.
+
+https://sv443.net/jokeapi/v2/
+
  
--- index.js
+++ index.js
@@ -1,5 +1,84 @@
-const fetch = require('isomorphic-fetch')
-const jokeURL = 'https://v2.jokeapi.dev/joke/Any?safe-mode'
+const baseURL = 'https://v2.jokeapi.dev/joke/Any?'
+
+const safeMode = 'safe-mode'
+const frenchMode = 'lang=fr'
+
+
+let setupHTML = document.getElementById('setup')
+let deliveryHTML = document.getElementById('delivery')
+
+
+const fetchJokes = async (event) => {
+    if(event.target.id === 'safeButton'){
+        const response = await  fetch(baseURL + safeMode).then(res => res.json())
+        
+         let setup = response.setup
+         let delivery = response.delivery
+         let joke = response.joke
+        
+        displayJokes(setup, delivery, joke)
+    } else if (event.target.id === 'frenchButton'){
+        const response = await  fetch(baseURL + frenchMode).then(res => res.json())
+        
+        let setup = response.setup
+        let delivery = response.delivery
+        let joke = response.joke
+       
+       displayJokes(setup, delivery, joke)
+     } else {
+        const response = await  fetch(baseURL).then(res => res.json())
+        
+        let setup = response.setup
+        let delivery = response.delivery
+        let joke = response.joke
+       
+       displayJokes(setup, delivery, joke)
+     }
+        
+ }
+
+
+
+function displayJokes (setup, delivery, joke){
+    if(setup){
+        console.log(setup, delivery)
+        setupHTML.innerText = setup
+        deliveryHTML.innerText = delivery
+    } else {
+        setupHTML.innerText = joke
+        deliveryHTML.innerHTML = ''
+    }
+}
+
+//French 
+
+const getFrenchJokes = async () => {
+    const response = await fetch(frenchURL).then(res => res.json())
+    // console.log(response)
+}
+
+
+let button = document.getElementById('jokeButton')
+button.addEventListener('click', fetchJokes)
+
+let safeButton = document.getElementById('safeButton')
+safeButton.addEventListener('click', fetchJokes)
+
+let frenchButton = document.getElementById('frenchButton')
+frenchButton.addEventListener('click', fetchJokes)
+
+/**
+ * put data in the variable 
+ * isolate  the information
+ * connect the two 
+ * send over the data to html
+ * button to call a new joke on very click 
+ * 
+ * 
+ * 
+ */
+fetchJokes()
+// getFrenchJokes()
 
 
 module.exports = fetchJokes
